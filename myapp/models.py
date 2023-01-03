0a6c63730c89d6fec4b9ccaa167f5fc73cab7870
pky-repos <panky.asg@gmail.com>
2023-01-03 12:49:26 +0530

    Added: job_id, task_id, job_started_at, job_end_at to JobLog; Calculate job runtime based on model fields

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class JobLog(models.Model):
-    id = models.IntegerField(primary_key=True)
-    task_runtime = models.IntegerField(null=True)  # millisecond
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    job_id = models.IntegerField()
+    task_id = models.UUIDField(null=True)
+    task_runtime = models.IntegerField(null=True)  # micro second
     task_status = models.CharField(max_length=20, null=True)
     misc = models.JSONField(null=True)
+    job_started_at = models.DateTimeField(null=True)
+    job_end_at = models.DateTimeField(null=True)
+
--- myapp/tasks.py
+++ myapp/tasks.py
@@ -1,9 +1,11 @@
 from celery import shared_task
+from django.db.models import F, Value
 
 from myapp.models import JobLog
 from time import time
 from celery.signals import task_prerun, task_postrun
 import random
+from datetime import datetime
 
 
 @shared_task
@@ -11,28 +13,30 @@ def job_predict_pending(pk):
     return random.random()
 
 
-d = {}
+# d = {}
 
 
 @task_prerun.connect
 def task_prerun_handler(signal, sender, task_id, task, args, kwargs, **extras):
     print("task_prerun_handler", task, args, kwargs, extras)
-    d[task_id] = time()
-    job_log = JobLog.objects.filter(pk=args[0])
+    # d[task_id] = time()
+    job_log = JobLog.objects.create(job_id=args[0], task_id=task_id, job_started_at=datetime.now())
     print(job_log)
 
 
 @task_postrun.connect
 def task_postrun_handler(signal, sender, task_id, task, args, kwargs, retval, state, **extras):
     print("task_postrun_handler", task, args, kwargs, retval, extras)
+    #
+    # try:
+    #     cost = time() - d.pop(task_id)
+    # except KeyError:
+    #     cost = -1
 
-    try:
-        cost = time() - d.pop(task_id)
-    except KeyError:
-        cost = -1
-
-    job_log = JobLog.objects.filter(pk=args[0]).update(
-        task_runtime=cost * 1000,
-        task_status=state
+    now = datetime.now()
+    job_log = JobLog.objects.filter(job_id=args[0], task_id=task_id).update(
+        task_status=state,
+        job_end_at=now,
+        task_runtime=Value(now) - F('job_started_at'),
     )
     print(job_log)

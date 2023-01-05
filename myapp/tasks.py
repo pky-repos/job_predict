12f862afef81e5d23ed1fe6858ac2158bf74b983
pky-repos <panky.asg@gmail.com>
2023-01-05 18:43:20 +0530

    Added: adding miscellaneous data to JobLog

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -1,7 +1,6 @@
 from celery import shared_task
 from django.db.models import F, Value
 from celery.signals import task_prerun, task_postrun
-import random
 from datetime import datetime
 
 from myapp.models import JobLog
@@ -9,12 +8,19 @@ from myapp.models import JobLog
 
 @shared_task
 def job_predict_pending(pk):
-    return random.random()
+    task_id = job_predict_pending.request.id
+
+    mydict = {"foo": "bar"}
+
+    job_log = JobLog.objects.filter(job_id=pk, task_id=task_id).update(
+        misc=mydict
+    )
+    print(job_log)
 
 
 @task_prerun.connect(sender=job_predict_pending)
 def task_prerun_handler(signal, sender, task_id, task, args, kwargs, **extras):
-    print("task_prerun_handler", task, args, kwargs, extras)
+    print("task_prerun_handler", task_id, task, args, kwargs, extras)
 
     job_log = JobLog.objects.create(job_id=args[0], task_id=task_id, job_started_at=datetime.now())
     print(job_log)
@@ -22,7 +28,7 @@ def task_prerun_handler(signal, sender, task_id, task, args, kwargs, **extras):
 
 @task_postrun.connect(sender=job_predict_pending)
 def task_postrun_handler(signal, sender, task_id, task, args, kwargs, retval, state, **extras):
-    print("task_postrun_handler", task, args, kwargs, retval, extras)
+    print("task_postrun_handler", task_id, task, args, kwargs, retval, extras)
 
     now = datetime.now()
     job_log = JobLog.objects.filter(job_id=args[0], task_id=task_id).update(
